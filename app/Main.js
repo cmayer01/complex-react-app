bb06d80c199b124416f4faf317729ecfd3a8abaf
cmayer <cmayer@furniturebankcoh.org>
2025-08-21 09:40:49 -0400

    Lesson 1

---
+++ app/Main.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>this is our app!</h1>
+      <p>The sky is blue.</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("app"))
+
+root.render(<ExampleComponent />)
