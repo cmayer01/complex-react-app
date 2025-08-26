933ff69c073320fc108a16c1a403b0d1fcae65b4
cmayer <cmayer@furniturebankcoh.org>
2025-08-26 13:27:07 -0400

    Use Context

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useContext } from "react"
 import { Link } from "react-router"
+import ExampleContext from "../ExampleContext"
 
 function HeaderLoggedIn(props) {
+  const { setLoggedIn } = useContext(ExampleContext)
+
   function handleLogout() {
-    props.setLoggedIn(false)
+    setLoggedIn(false)
     localStorage.removeItem("complexAppToken")
     localStorage.removeItem("complexAppAvatar")
     localStorage.removeItem("complexAppUsername")
